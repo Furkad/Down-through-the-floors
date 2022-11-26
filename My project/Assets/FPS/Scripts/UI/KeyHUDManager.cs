ef8f68a5d356083873c4ea0ce6a219018f6f777e
Furkad <71381867+Furkad@users.noreply.github.com>
2022-11-27 01:14:28 +0300

    Some animations and pickups

---
+++ My project/Assets/FPS/Scripts/UI/KeyHUDManager.cs
@@ -0,0 +1,37 @@
+using Unity.FPS.Game;
+using Unity.FPS.Gameplay;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unity.FPS.UI
+{
+    public class KeyHUDManager : MonoBehaviour
+    {
+        KeyPickup m_KeyPickup;
+        PlayerCharacterController m_PlayerCharacterController;
+        [Tooltip("Image of a key in UI")]
+        public Image keyImage;
+
+        void Start()
+        {
+            m_PlayerCharacterController = FindObjectOfType<PlayerCharacterController>();
+            DebugUtility.HandleErrorIfNullFindObject<PlayerCharacterController, KeyHUDManager>(m_PlayerCharacterController, this);
+
+            m_KeyPickup = FindObjectOfType<KeyPickup>();
+            DebugUtility.HandleErrorIfNullFindObject<KeyPickup, KeyHUDManager>(m_KeyPickup, this);
+
+            if (m_PlayerCharacterController.HasKey)
+                keyImage.enabled = true;
+            else
+                keyImage.enabled = false;
+
+            m_KeyPickup.OnKeyStateSwitch += SwitchImageState;
+        }
+
+        void SwitchImageState(bool state)
+        {
+            keyImage.enabled = state;
+        }
+
+    }
+}
