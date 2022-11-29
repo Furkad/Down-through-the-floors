32198655e08590897635a30279b3a10e39b6e3ed
Lichitek <71435373+Lichitek@users.noreply.github.com>
2022-11-29 16:28:41 +0300

    endOfRoom

--- My project/Assets/LevelGenerator/Scripts/LevelGenerator.cs
+++ My project/Assets/LevelGenerator/Scripts/LevelGenerator.cs
@@ -9,44 +9,20 @@ namespace LevelGenerator.Scripts
 {
     public class LevelGenerator : MonoBehaviour
     {
-        /// <summary>
-        /// LevelGenerator seed
-        /// </summary>
         public int Seed;
 
-        /// <summary>
-        /// Container for all sections in hierarchy
-        /// </summary>
         public Transform SectionContainer;
 
-        /// <summary>
-        /// Maximum level size measured in sections
-        /// </summary>
         public int MaxLevelSize;
 
-        /// <summary>
-        /// Maximum allowed distance from the original section
-        /// </summary>
         public int MaxAllowedOrder;
 
-        /// <summary>
-        /// Spawnable section prefabs
-        /// </summary>
         public Section[] Sections;
 
-        /// <summary>
-        /// Spawnable dead ends
-        /// </summary>
         public DeadEnd[] DeadEnds;
 
-        /// <summary>
-        /// Tags that will be taken into consideration when building the first section
-        /// </summary>
         public string[] InitialSectionTags;
         
-        /// <summary>
-        /// Special section rules, limits and forces the amount of a specific tag
-        /// </summary>
         public TagRule[] SpecialRules;
 
         protected List<Section> registeredSections = new List<Section>();
